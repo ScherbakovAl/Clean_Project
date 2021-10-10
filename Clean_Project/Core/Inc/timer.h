f1a8bdba251dba24b09401cd64fa2fe78d6368ca
Scherbakov_AL <72318856+ScherbakovAl@users.noreply.github.com>
2021-10-10 21:45:54 +0300

    ti

--- Clean_Project/Core/Inc/timer.h
+++ Clean_Project/Core/Inc/timer.h
@@ -7,7 +7,7 @@
 // CPU and SysTick timer frequency
 #define F_CPU 		            72000000UL      // 72MHz
 // every 0.1 ms
-#define SYS_TIMER_TICK  	    (F_CPU/40000-1)  // 10000 Hz
+#define SYS_TIMER_TICK  	    (F_CPU/40000-1)  // 40000 Hz
 
 // Ticks
 extern volatile uint32_t sys_tick;
